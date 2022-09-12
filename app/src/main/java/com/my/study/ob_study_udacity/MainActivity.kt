46bf8e7fd8c1410a779e0a899443a3d28608a63a
권용민 <briandr@naver.com>
2022-09-12 17:09:10 +0900

    make roll dice image feature

--- app/src/main/java/com/my/study/ob_study_udacity/MainActivity.kt
+++ app/src/main/java/com/my/study/ob_study_udacity/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.my.study.ob_study_udacity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
+
+/*
+The context object allows you to communicate with and get information about the current state of Android Oerating System
+ */
