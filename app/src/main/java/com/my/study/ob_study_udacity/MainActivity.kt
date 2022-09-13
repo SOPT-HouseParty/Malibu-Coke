af781931455451ea914d73a002eb1a84c01d605b
권용민 <briandr@naver.com>
2022-09-13 20:33:03 +0900

    make button logic and change to databinding

--- app/src/main/java/com/my/study/ob_study_udacity/MainActivity.kt
+++ app/src/main/java/com/my/study/ob_study_udacity/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.my.study.ob_study_udacity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.my.study.ob_study_udacity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        with(binding) {
+            nicknameText.text = nicknameEdit.text
+            //invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
