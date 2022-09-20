b487579aebba853f009e9780db13f01a7f0ab72b
권용민 <briandr@naver.com>
2022-09-20 18:42:05 +0900

    main activity 무슨 코든지 모르겠지만 일단 따라적음

--- app/src/main/java/com/my/study/ob_study_udacity/MainActivity.kt
+++ app/src/main/java/com/my/study/ob_study_udacity/MainActivity.kt
@@ -3,6 +3,8 @@ package com.my.study.ob_study_udacity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.my.study.ob_study_udacity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.mainNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.mainNavHostFragment)
+        return navController.navigateUp()
     }
 }
