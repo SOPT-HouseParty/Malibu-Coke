e057e479ac168a720591c89b60cfcc59173ed0a9
권용민 <briandr@naver.com>
2022-09-20 18:41:21 +0900

    game won fragment

--- app/src/main/java/com/my/study/ob_study_udacity/GameWonFragment.kt
+++ app/src/main/java/com/my/study/ob_study_udacity/GameWonFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.my.study.ob_study_udacity.databinding.FragmentGameWonBinding
 
 class GameWonFragment : Fragment() {
@@ -15,6 +16,9 @@ class GameWonFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentGameWonBinding>(layoutInflater, R.layout.fragment_game_won, container, false)
+        binding.nextMatchButton.setOnClickListener{
+            it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
