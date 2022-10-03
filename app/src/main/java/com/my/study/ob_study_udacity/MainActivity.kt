cf6097386ec3907617c7b56da644a198875f52a6
권용민 <briandr@naver.com>
2022-10-03 18:36:36 +0900

    아몰라 커밋

--- app/src/main/java/com/my/study/ob_study_udacity/MainActivity.kt
+++ app/src/main/java/com/my/study/ob_study_udacity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.my.study.ob_study_udacity.databinding.ActivityMainBinding
 
@@ -12,8 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        //val navController = this.findNavController(R.id.mainNavHostFragment)
-        //NavigationUI.setupActionBarWithNavController(this, navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
